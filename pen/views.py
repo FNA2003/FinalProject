0f21a78e3f4384c71474f2037382f2fd9563ceac
Franco <franco.angeletti01@gmail.com>
2022-04-30 16:35:37 -0300

    Starting to create the index page

--- pen/views.py
+++ pen/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "pen/codeMaker.html")
+    return render(request, "pen/index.html")
