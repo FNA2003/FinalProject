c389d46fe7990a1e7f879c9762ae9507ef6579a9
Franco <franco.angeletti01@gmail.com>
2022-05-21 21:01:23 -0300

    A

--- pen/views.py
+++ pen/views.py
@@ -15,13 +15,17 @@ def index(request):
 def register(request):
     if request.method == "GET":
         return render(request, "pen/register.html")
-    else:
+    elif request.method == "POST":
         if ("user" not in request.POST or "email" not in request.POST or "password" 
             not in request.POST or "confirm" not in request.POST):
             return render(request, "pen/register.html", { "message":"You have to complete all of the fields" })
         
         if (request.POST["password"] != request.POST["confirm"]):
             return render(request, "pen/register.html", { "message":"The passwords must match" })
+
+        for i in User.objects.values_list("username"):
+            if i[0] == request.POST["user"]:
+                return render(request, "pen/register.html", { "message":"The username is taken" })  
 
         newUser = User(username=request.POST["user"], email=request.POST["email"], password=request.POST["password"])
 
@@ -35,7 +39,20 @@ def register(request):
 
 
 def logIn(request):
-    return render(request, "pen/logIn.html")
+    if request.method == "GET":
+        return render(request, "pen/logIn.html")
+    elif request.method == "POST":
+        if "user" not in request.POST or "password" not in request.POST:
+            return render(request, "pen/logIn.html", {"message":"AN INPUT('S) WAS NOT PROVIDED"})
+
+        password = request.POST["password"]
+        username = request.POST["user"]
+
+        user = authenticate(request, username=username, password=password)
+
+        # The user is always None, why?
+
+        return HttpResponseRedirect(reverse("index"))
 
 
 
