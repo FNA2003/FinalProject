19f0027d9b0c13a7fcd4c12bde263cd832794fd1
Franco <franco.angeletti01@gmail.com>
2022-05-24 23:37:12 -0300

    Making the pop up for the iframe view in the home page

--- pen/static/pen/index.js
+++ pen/static/pen/index.js
@@ -1,7 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {    
+    let state = false;
     scroll(0,0);
 
     document.querySelector("#goUp").style.display = "none";
+
+    document.querySelectorAll(".projectHolder").forEach(holder => {
+        holder.addEventListener("click", () => {
+            let popUp = document.querySelector("#window");
+
+            if (state === false){
+                popUp.style.cssText = `display:block;
+                                    position: absolute;
+                                    top:  calc(9vh + ${window.scrollY}px);
+                                    left: 25vw;
+                                    width: 50vw;
+                                    height: 90vh;
+                                    background: red;`;
+
+                state = true;
+            } else {
+                popUp.style.cssText = `display: none;`;
+                state = false;
+            }
+        });
+    });
 });
 
 document.addEventListener("scroll", () => {
